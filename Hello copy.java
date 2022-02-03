77d749b7395a00a1f7a7ba07710530fa90232c30
femmy22 <98857794+femmy22@users.noreply.github.com>
2022-02-03 17:54:56 -0500

    Hello.java

---
+++ Hello copy.java
@@ -0,0 +1,13 @@
+/**********************************************
+* Lab 1
+***********************************************
+* Femi Adepoju
+* 01-26-2022
+* CMSC 255 C90
+*********************************/
+
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("Hello Femi");
+	}
+}
